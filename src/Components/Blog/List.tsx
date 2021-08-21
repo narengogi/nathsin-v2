850cf09fc091ff5c9ff92367dce310808f257183
narendranath gogineni <narenrockstar1@gmail.com>
2021-08-22 00:07:09 +0530

    randomness

--- src/Components/Blog/List.tsx
+++ src/Components/Blog/List.tsx
@@ -5,8 +5,10 @@ import { helloWorld } from './Posts/helloWorld';
 import { information } from './Posts/information';
 import { moreOnTheLoops } from './Posts/moreOnTheLoops';
 import { takingABreak } from './Posts/break';
+import { randomness } from './Posts/randomness';
 
 export const allPosts: any = [
+  { title: 'In Defense of Randomness', date: '17th August 2021', href: '/randomness', content: randomness() },
   { title: 'Unvaried Iterations', date: '22nd May 2021', href: '/unvaried', content: unvaried() },
   { title: 'Quantifying Information', date: '24th March 2021', href: '/information', content: information() },
   { title: 'Dealing With Absence', date: '17th Jan 2021', href: '/absence', content: absence() },
---
+++ src/Components/Blog/Posts/randomness.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export function randomness() {
+  return (
+    <div>
+      <div className='para'>
+        Well okay, I do often say that recommendation systems are pure evil, and it is also true that most of the things that I know, I wouldn’t have, were it not for algorithmic recommendations. But
+        while those recommendations have shaped my understanding of the world at large, I think it is safe to say that they did not do so with an agenda in mind. This is an important distinction to
+        make and my ability do so is largely circumstantial in that I am lucky to have grown up when personalised feeds (personalised by the intermediaries that is) were largely user configurable and
+        reflected the general content preferences of the population on the internet, who were but an insignificant subset of the all the groups that shaped culture at that point in time.
+      </div>
+      <div className='para'>
+        I do not know for certain if my expertise on the Iranian revolution or my knowledge of the exact details of all the spices in different kinds of biryani is ever going to be of any use to me,
+        but in the end it does make me who I am. Even though I could dismiss this knowledge and assume for a moment that the time spent on them could have been used better in pursuing planned
+        endeavours, I’d be stupid to discount the interconnectedness of it all.
+      </div>
+      <div className='para'>
+        One’s own mind really is the most important information processor in one’s possession. While it is only the physical that separates electronic processors from us, the subconscious does a poor
+        job at making that distinction. This is where an underexplored problem lies. While the mind is great at perceiving the present, the fidelity of reproduction of these everyday electronic
+        devices is far, far better. And because we learn through reinforcement (in loops and loops and loops), consuming content that subtly shifts the loops but only to lead you to a predestined goal
+        should be classified as nothing other than being dangerously manipulative.
+      </div>
+      <div className='para'>
+        This post is titled so because I do not really believe that there is any such thing as free will, as in that there isn’t really any action one could perform whose trailing set of actions (and
+        in turn their preceding ones, and so on) had all originated in the subjects own mind without the subjects environment dictating the course to a certain degree.{' '}
+      </div>
+      <div className='para'>So then, what is an acceptable degree of randomness? </div>
+    </div>
+  );
+}
