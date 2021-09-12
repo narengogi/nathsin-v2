74ce4af99bacea64f88faae112eab3fa11e8d5fa
narendranath gogineni <narenrockstar1@gmail.com>
2021-09-12 11:38:38 +0530

    minor fix

--- src/Components/Blog/Posts/helloWorld.tsx
+++ src/Components/Blog/Posts/helloWorld.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 export function helloWorld() {
   return (
     <div>
-      <div className='para'>
-        I have no idea how to begin writing this post, I've been writing and deleting whole lines for atleast half an hour. my own mindless social media consumption (To be honest I try to finish this
-        one again and again every 2-3 months).
-      </div>
+      <div className='para'>I have no idea how to begin writing this post, I've been writing and deleting whole lines for atleast half an hour.</div>
       <div className='para'>
         The problem with human memory in my opinion, is that while it is amazing in it's ability to record and store attributes separately to the tokens of information associated, the fidelity of
         reproduction unlike from mechanical memory is terribly poor. Whenever I've tried reminiscing good memories, I've always felt limited by the capacity of my own mind. Surely, the photographs and
