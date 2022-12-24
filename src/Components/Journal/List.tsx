dc7577f22487b665462878a2de6d584921a53543
narendranath gogineni <narenrockstar1@gmail.com>
2022-12-25 03:05:06 +0530

    Add back other tab

--- src/Components/Journal/List.tsx
+++ src/Components/Journal/List.tsx
@@ -1,11 +1,3 @@
-import {Time} from "./Posts/Time";
-import {ModellingTheBrain} from "./Posts/ModellingTheBrain";
-import {Friends} from "./Posts/Friends";
-import {Smoke} from "./Posts/Smoke";
+import { ModellingTheBrain } from './Posts/ModellingTheBrain';
 
-export const allPosts: any = [
-    { title: 'Time', date: '6th August 2019', href: '/time', content: Time() },
-    { title: 'Modelling The Brain', date: '11th August 2020', href: '/brain', content: ModellingTheBrain() },
-    { title: 'Friends', date: '21st September 2019', href: '/friends', content: Friends() },
-    { title: 'The First Time I smoked', date: '29th September 2018', href: '/smoke', content: Smoke() },
-];
+export const allPosts: any = [{ title: 'Modelling The Brain', date: '11th August 2020', href: '/brain', content: ModellingTheBrain() }];
