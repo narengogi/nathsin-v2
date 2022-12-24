49556452d2a9f0b31fbf99a4cd0a20995493ded8
narendranath gogineni <narenrockstar1@gmail.com>
2022-12-25 02:39:23 +0530

    Add RSS

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as RssIcon } from '../../assets/images/rss.svg';
+
 import '../Common/common.css';
 
 function Footer() {
@@ -7,6 +9,10 @@ function Footer() {
       <div style={{ justifySelf: 'flex-start' }}>
         <a className='href' href='https://github.com/nathsin/nathsin-v2'>
           &#64; NNG '{new Date().getFullYear().toString().slice(2)}
+        </a>
+        &nbsp;
+        <a href='https://naths.in/rss.xml'>
+          <RssIcon width='0.8rem' alt='RSS icon' style={{ marginBottom: '-2px' }} />
         </a>
       </div>
     </div>
