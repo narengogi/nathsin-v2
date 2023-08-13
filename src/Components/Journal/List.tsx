0a82c9dcb464a3491937c2554c1a99a2499358c4
Naren Gogineni <92291785+narenqlm@users.noreply.github.com>
2023-08-14 01:15:27 +0530

    Add an old note

--- src/Components/Journal/List.tsx
+++ src/Components/Journal/List.tsx
@@ -1,3 +1,14 @@
-import { ModellingTheBrain } from './Posts/ModellingTheBrain';
+import {ModellingTheBrain} from './Posts/ModellingTheBrain';
+import {DataMap} from "./Posts/DataMap";
 
-export const allPosts: any = [{ title: 'Modelling The Brain', date: '11th August 2020', href: '/brain', content: ModellingTheBrain() }];
+export const allPosts: any = [{
+    title: 'Data Map',
+    date: '17th July 2022',
+    href: '/datamap',
+    content: DataMap()
+}, {
+    title: 'Modelling The Brain',
+    date: '11th August 2020',
+    href: '/brain',
+    content: ModellingTheBrain()
+}];
---
+++ src/Components/Journal/Posts/DataMap.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function DataMap() {
+    return (
+        <div>
+            <div className='para'>On some days, the mind resembles a bazaar in Old Hyderabad, there is no coherence to
+                the ideas reaching the most abstract layer, there’s no thread attached that I could use to trace their
+                origins. The chemicals conspire to push ahead what could potentially satisfy their collective needs (in
+                groups).
+            </div>
+            <div className='para'>
+                Meditation never helps, I find locking in the latest idea at hand and starting work on it allows me to
+                judge it’s priority and then rearrange the tasks, noting them down and then ranking them also helps.
+            </div>
+            <div className='para'> I’m working on a fun project these days, this has been on my mind for a bit. I’ll be
+                mapping as much of the data about me and my interactions that are recorded digitally as possible. The
+                idea is to link them to generate insights. Quite a few interesting challenges there, I’ll update with a
+                blog post once done if time permits. There’s another smaller project I want to be doing.
+            </div>
+        </div>
+    );
+}
