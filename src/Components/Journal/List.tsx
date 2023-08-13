dd6eba7050c6b30b65520c85d48f5817031f48fc
Naren Gogineni <92291785+narenqlm@users.noreply.github.com>
2023-08-14 01:33:28 +0530

    Add an old blog

--- src/Components/Journal/List.tsx
+++ src/Components/Journal/List.tsx
@@ -1,11 +1,17 @@
 import {ModellingTheBrain} from './Posts/ModellingTheBrain';
 import {DataMap} from "./Posts/DataMap";
+import {Waste} from "./Posts/Waste";
 
 export const allPosts: any = [{
     title: 'Data Map',
     date: '17th July 2022',
     href: '/datamap',
     content: DataMap()
+},{
+    title: `A Waste of Time`,
+    date: '27th December 2020',
+    href: '/waste',
+    content: Waste()
 }, {
     title: 'Modelling The Brain',
     date: '11th August 2020',
---
+++ src/Components/Journal/Posts/Waste.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export function Waste() {
+    return (
+        <div>
+            <div className='para'>At this point I don’t know what to do, except that I need to be working towards a goal
+                I cannot clearly define myself. The conflict and the resulting uncertainty arises from the
+                incompatibility of the long term agenda with what appears to be the most reasonable thing to do at the
+                present moment with the available inputs. But if in pursuit of the required inputs (Which I must say is
+                a necessary and useful thing to do with your time) one does not employ the inputs he is already in
+                possession of, the efficacy of such a system is worrisome and not just that, such a process also
+                represents a distressing divorce from reality. Now I see clearly.
+
+            </div>
+            <div className='para'>
+                I realise, that as an individual, my attention span is my most valuable asset, for the last two weeks
+                I’ve not done anything I can sell as having spent the time wisely. In fact it isn’t even that. The last
+                two weeks have just slipped from under my feet and I can’t seem to remember how that time was spent, or
+                recall any worthwhile discussion that I’d want to remember after a few months as well. Wait wait,
+                scratch all that, this contributes little to the discussion, what I really wanted to resolve here is,
+                how the heart has been able to get away with wasting all that time. This represents in my understanding
+                a death of conscious thought. Which is a terrible terrible thing, for there is nothing in this world not
+                achieved through conscious thought that stays with you for long. What could be a solution?
+            </div>
+        </div>
+    );
+}
