969ad1ebff494f30cee7dc2d49da08a1a651eb5f
Naren Gogineni <92291785+narenqlm@users.noreply.github.com>
2023-08-19 21:03:32 +0530

    Add an old blog

--- src/Components/Blog/List.tsx
+++ src/Components/Blog/List.tsx
@@ -1,23 +1,25 @@
-import { unvaried } from './Posts/unvaried';
-import { absence } from './Posts/absence';
-import { futility } from './Posts/futility';
-import { helloWorld } from './Posts/helloWorld';
-import { information } from './Posts/information';
-import { moreOnTheLoops } from './Posts/moreOnTheLoops';
-import { takingABreak } from './Posts/break';
-import { randomness } from './Posts/randomness';
-import { onLove } from './Posts/onLove';
-import { memeTree } from './Posts/memeTree';
+import {unvaried} from './Posts/unvaried';
+import {absence} from './Posts/absence';
+import {futility} from './Posts/futility';
+import {helloWorld} from './Posts/helloWorld';
+import {information} from './Posts/information';
+import {moreOnTheLoops} from './Posts/moreOnTheLoops';
+import {takingABreak} from './Posts/break';
+import {randomness} from './Posts/randomness';
+import {onLove} from './Posts/onLove';
+import {memeTree} from './Posts/memeTree';
+import {die} from "./Posts/die";
 
 export const allPosts: any = [
-  { title: 'Meme Tree', date: '20th February 2023', href: '/meme-tree', content: memeTree() },
-  { title: 'On Love', date: '17th November 2022', href: '/love', content: onLove() },
-  { title: 'In Defense of Randomness', date: '17th August 2021', href: '/randomness', content: randomness() },
-  { title: 'Unvaried Iterations', date: '22nd May 2021', href: '/unvaried', content: unvaried() },
-  { title: 'Quantifying Information', date: '24th March 2021', href: '/information', content: information() },
-  { title: 'Dealing With Absence', date: '17th Jan 2021', href: '/absence', content: absence() },
-  { title: `I'm Taking A Break`, date: '14th August 2020', href: '/break', content: takingABreak() },
-  { title: 'Futility', date: '24th July 2020', href: '/futility', content: futility() },
-  { title: 'More On The Loops', date: '13th July 2020', href: '/loops', content: moreOnTheLoops() },
-  { title: 'Hello World', date: '07th July 2020', href: '/hello-world', content: helloWorld() },
+    {title: 'Thoughts on death', date: '19th august 2023', href: '/death', content: die()},
+    {title: 'Meme Tree', date: '20th February 2023', href: '/meme-tree', content: memeTree()},
+    {title: 'On Love', date: '17th November 2022', href: '/love', content: onLove()},
+    {title: 'In Defense of Randomness', date: '17th August 2021', href: '/randomness', content: randomness()},
+    {title: 'Unvaried Iterations', date: '22nd May 2021', href: '/unvaried', content: unvaried()},
+    {title: 'Quantifying Information', date: '24th March 2021', href: '/information', content: information()},
+    {title: 'Dealing With Absence', date: '17th Jan 2021', href: '/absence', content: absence()},
+    {title: `I'm Taking A Break`, date: '14th August 2020', href: '/break', content: takingABreak()},
+    {title: 'Futility', date: '24th July 2020', href: '/futility', content: futility()},
+    {title: 'More On The Loops', date: '13th July 2020', href: '/loops', content: moreOnTheLoops()},
+    {title: 'Hello World', date: '07th July 2020', href: '/hello-world', content: helloWorld()},
 ];
---
+++ src/Components/Blog/Posts/die.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export function die() {
+    return (
+        <div>
+            <div className='para'>
+                My thathayya killed himself last December. He was 75. There’s this story about how he used to be pretty
+                normal until one day in his 30’s he spent way too much time at the vallakaadu. My parents
+                and the relatives got it registered as a natural death, as if there is no honour in killing yourself.
+                They did it to save themselves from the guilt of having let it happen. I knew he’d been very depressed
+                since ammammas death. I keep thinking that nothing made sense to him anymore. But I don’t know what had
+                changed for him in those 8 years for this to happen.
+            </div>
+            <div className='para'>
+                I often think about starving to death, or about how that act would symbolise the supremacy of the mind.
+                But if life is meaningless, so is death. The conflict must continue. Systems rearrange to serve superior
+                memes, rules are drawn to explain them, systems of formal logic emerge, and the conflicts emerge again,
+                because the systems are impure now, the subsystems contradict each other.
+            </div>
+            <div className='para'>
+                The day I read Camus, I knew that my system of formal logic was shattered by a spear
+                of proof. The layers of abstraction that were built in my mind on top of the axiom that everything is
+                for the multiplication and survival of the gene (as a set of instructions) crumbled. Because ME as an
+                entity is far removed from the gene. I’m a meme, the one that translates between memes and genes.
+            </div>
+            <div className='para'>
+                So let’s put aside people who kill themselves because survival looks too hard for
+                them. What about people who kill themselves when everything is provided for? The meme continues
+                surviving by having asserted its primacy. If the goal is to find meaning and in not finding it, the meme
+                terminates the host it operates on, but it implants itself in others, by having them reason about why it
+                terminated itself.
+            </div>
+        </div>
+    );
+}
