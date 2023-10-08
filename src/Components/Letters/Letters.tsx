c010d7a93b440e021605b68e6f3a1c5a8dcf7896
Narendranath Gogineni <47327611+narengogi@users.noreply.github.com>
2023-10-08 23:25:00 +0530

    Update Letters.tsx

--- src/Components/Letters/Letters.tsx
+++ src/Components/Letters/Letters.tsx
@@ -6,7 +6,7 @@ function Letters() {
     <div className='centeredContainer'>
       <div style={{ fontSize: '24px', color: 'var(--brightCyan)', paddingTop: '2rem' }}>Send me a Post!</div>
       <div style={{ padding: '2rem 0' }}>"The world is full of paper. Write to me.” - Agha Shahid Ali</div>
-      <div style={{ fontSize: '12px', color: 'var(--yellow)' }}>C-106, Ittina Anai, Kempapura Main Road, Bellandur - 560037</div>
+      <div style={{ fontSize: '12px', color: 'var(--yellow)' }}>#240, Sapthagiri Colony, Kukatpally, Hyderabad - 500072</div>
     </div>
   );
 }
