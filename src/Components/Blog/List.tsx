49675053259e7082938bf8ffb616587e2763c0e3
Naren Gogineni <92291785+narenqlm@users.noreply.github.com>
2023-12-14 22:30:42 +0530

    New post

--- src/Components/Blog/List.tsx
+++ src/Components/Blog/List.tsx
@@ -8,10 +8,12 @@ import {takingABreak} from './Posts/break';
 import {randomness} from './Posts/randomness';
 import {onLove} from './Posts/onLove';
 import {memeTree} from './Posts/memeTree';
+import {uneasiness} from "./Posts/uneasiness";
 import {die} from "./Posts/die";
 
 export const allPosts: any = [
     // {title: 'Thoughts on death', date: '19th august 2023', href: '/death', content: die()},
+    {title: 'Uneasiness in being', date: '14th December 2023', href: '/being', content: uneasiness()},
     {title: 'Meme Tree', date: '20th February 2023', href: '/meme-tree', content: memeTree()},
     {title: 'On Love', date: '17th November 2022', href: '/love', content: onLove()},
     {title: 'In Defense of Randomness', date: '17th August 2021', href: '/randomness', content: randomness()},
---
+++ src/Components/Blog/Posts/uneasiness.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export function uneasiness() {
+    return (
+        <div>
+            <div className='para'>
+                I keep thinking that perhaps meditation will fix me, and then I think about something else that is not
+                about fixing my core abstractions, and then I think about something that a finite automata modeled after
+                those core abstractions would not recognize in its domain. And then I loop, working on ideas that no
+                longer interest me, ideas that are not my own. I love randomness, and I’ve long held the view that it
+                helps to have thinly connected nodes that you can pull up to reason. But I’ve grown weary, and I
+                feel trapped.
+            </div>
+            <div className='para'>
+                When I say core abstractions, I refer to those clusters of ideas that nearly every sensory input seems
+                to appeal to. And not just appeal, these clusters are reinforced with every input that passes by, and
+                how those inputs are fitted into the idea-cloud is also heavily directed by these clusters. If you try
+                to explain your uneasiness in being by tracing a finger along the neural pathways from what you do to
+                the ideas that define you, you could easily see where the problem lies. And the only fix is to shorten
+                that path.
+            </div>
+        </div>
+    )
+}
