c9025e614e1b9b34a86c5f0dbaeef36b375e259c
Narendranath Gogineni <47327611+narengogi@users.noreply.github.com>
2023-12-16 13:13:35 +0530

    Update uneasiness.tsx

--- src/Components/Blog/Posts/uneasiness.tsx
+++ src/Components/Blog/Posts/uneasiness.tsx
@@ -15,7 +15,7 @@ export function uneasiness() {
                 When I say core abstractions, I refer to those clusters of ideas that nearly every sensory input seems
                 to appeal to. And not just appeal, these clusters are reinforced with every input that passes by, and
                 how those inputs are fitted into the idea-cloud is also heavily directed by these clusters. If you try
-                to explain your uneasiness in being by tracing a finger along the neural pathways from what you do to
+                to explain your uneasiness in being by tracing a finger along the neural pathways from what you do, to
                 the ideas that define you, you could easily see where the problem lies. And the only fix is to shorten
                 that path.
             </div>
