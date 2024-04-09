d114ca3c430891d7ed2d7abb23c18f4fb7d567cb
Narendranath Gogineni <47327611+narengogi@users.noreply.github.com>
2024-04-10 00:15:59 +0530

    Remove duplicate

--- src/Components/Blog/List.tsx
+++ src/Components/Blog/List.tsx
@@ -16,7 +16,6 @@ export const allPosts: any = [
     {title: 'abstractions', date: '9th April 2024', href: '/abstractions', content: abstractions()},
     // {title: 'Thoughts on death', date: '19th august 2023', href: '/death', content: die()},
     {title: 'Uneasiness in being', date: '14th December 2023', href: '/being', content: uneasiness()},
-    {title: 'Uneasiness in being', date: '14th December 2023', href: '/being', content: uneasiness()},
     {title: 'Meme Tree', date: '20th February 2023', href: '/meme-tree', content: memeTree()},
     {title: 'On Love', date: '17th November 2022', href: '/love', content: onLove()},
     {title: 'In Defense of Randomness', date: '17th August 2021', href: '/randomness', content: randomness()},
