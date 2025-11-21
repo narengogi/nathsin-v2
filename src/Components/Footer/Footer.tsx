bf69b6d5a4ea33ab73b130ebf1ab2fb498cf0748
Narendranath Gogineni <47327611+narengogi@users.noreply.github.com>
2025-11-22 03:27:24 +0530

    Update Footer.tsx

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <div className='footer'>
       <div style={{ justifySelf: 'flex-start' }}>
         <a className='href' href='https://github.com/nathsin/nathsin-v2'>
-          &#64; NNG '{new Date().getFullYear().toString().slice(2)}
+          &#64; RSS
         </a>
         &nbsp;
         <a href='https://naths.in/rss.xml'>
