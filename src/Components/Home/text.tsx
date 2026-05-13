012e8ef5e06fe9e9c1f5691d70513527460d3401
Narendranath Gogineni <47327611+narengogi@users.noreply.github.com>
2026-05-13 22:51:06 +0530

    Update text.tsx

--- src/Components/Home/text.tsx
+++ src/Components/Home/text.tsx
@@ -1,5 +1,4 @@
 export const welcome: string = `Hey There!! Welcome to my space on the Internet!`;
 export const about: string = `I'm Naren, I umm......do stuff. In my free time, I conduct amateur 
-research about the chaos theory and explore cellular automaton. I'd like to one day be able to 
-crack the code to emergent self conscious life. Well apart from this, I work at a smoll startup building cool things!`;
+research about the chaos theory and explore cellular automaton and build things (interactive interfaces and web servers). Well apart from this, I work at a smoll startup building cool things!`;
 export const more: string = `If you're looking for code, you can check out my GitHub profile here ---->`
