a7416f34a63d4a48f78a706e827fa88f39fb1cc3
Narendranath Gogineni <narenrockstar1@gmail.com>
2026-08-15 19:38:37 -0700

    add goodreads to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { ReactComponent as RssIcon } from '../../assets/images/rss.svg';
-
-import '../Common/common.css';
+import React from "react";
+import { ReactComponent as RssIcon } from "../../assets/images/rss.svg";
+import { ReactComponent as GoodreadsIcon } from "../../assets/images/goodreads.svg";
+import "../Common/common.css";
 
 function Footer() {
   return (
-    <div className='footer'>
-      <div style={{ justifySelf: 'flex-start' }}>
-        <a href='https://naths.in/rss.xml'>
-             RSS&nbsp;<RssIcon width='0.8rem' alt='RSS icon' style={{ marginBottom: '-2px' }} />
-        </a>
-      </div>
+    <div className="footer">
+      <a href="https://naths.in/rss.xml">
+        <RssIcon
+          width="0.8rem"
+          alt="RSS icon"
+          style={{ marginBottom: "-2px", marginRight: "0.5rem" }}
+        />
+        RSS
+      </a>
+      <a href="https://www.goodreads.com/user/show/108597085-naren">
+        <GoodreadsIcon
+          width="0.8rem"
+          alt="Goodreads icon"
+          style={{ marginBottom: "-2px", marginRight: "0.5rem" }}
+        />
+        Goodreads
+      </a>
     </div>
   );
 }
