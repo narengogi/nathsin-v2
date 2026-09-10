8021b65b9f6df6305a933e6d17600493039fc15f
Narendranath Gogineni <narenrockstar1@gmail.com>
2026-09-10 11:34:28 -0700

    lesswrong

--- src/Components/Blog/Posts/lesswrong.tsx
+++ src/Components/Blog/Posts/lesswrong.tsx
@@ -5,21 +5,32 @@ export function lesswrong() {
     <div>
       <div className='para'>
         Language shapes how we process and partition stimulus in our minds. Our use of speech, our literature, in effect is an attempt to impress one's state as well as is intended and possible within
-        their understanding of the workings of their environment.
-      </div>
-      <div className='para'>
-        Richard Dawkins coined the term <code>meme</code> to denote a unit of information, information meant for cultural transmission and imitation. It would be neigh impossible to quantify embedded
+        their model of the workings of the environment. Richard Dawkins coined the term <code>meme</code> to denote a unit of information, information meant for cultural transmission and imitation. It would be neigh impossible to quantify embedded
         context without constructing the environment that the information maps to. Ideas like religion permeate, often morphing to fit the changing environment and persist. There are ideas that do
         not survive newer conditions, some that persist despite, some that only remain as salavaged boards making up a reconstructed ship.
       </div>
       <div className='para'>
-        When I quiz my friends on if they believe consciousness to be a solved problem and how they would rate it on a scale of 1-5, the distribution is bi-modal, people either believe that there is
+        When I ask my friends on if they believe consciousness to be a solved problem and how they would rate it on a scale of 1-5, the distribution is bi-modal, people either believe that there is
         something fundamentally unique within their substrate that cannot be be replicated or that we are only limited by compute in what otherwise is a solved problem. There comes a moment in
         everyones lives, only if fleeting for some, where they question if people around them really are conscious, or merely react to the stimuli that surrond them. With most hypotheticals, we figure
         there is no right answer, we settle for what amounts to be the lesswrong choice.
       </div>
       <div className='para'>
-        A lesswrong answer to me is if my arguments to justify my choice do not contradict within themselves. That the matter is tractable in my mind.
+        A lesswrong answer to me is if the set of arguments to justify my choice do not contradict within themselves. That the matter is sufficiently tractable in my mind.
+        Of late, a favourite pass-time of mine has been posing hypotheticals in groups, asking people what they would do in scenarios that I would not have clear answers to,
+        This question has been going around the EA community recently: "How many shrimp would you be willing to sacrifice to save a human life".
+        For context, we consume a few trillion shrimp every year. Often I do not want to answer these questions. It is frustrating being put in that position.
+        But again, it is just a question and you should be able to take a stand in intellectual pursuit.
+      </div>
+      <div className='para'>
+        Never quote me on this, but I'd not willingly switch shrimp for a person unless I know them. I've had people argue in favour of the switch,
+        that giving up as many shrimp as 1 person eats every year is fine, or 10 or all of the shrimp consumed every year everywhere. 
+        To avoid group think, it should be sufficient that the answer be explainable to you, and that you be open to desconstructing your setup.
+      </div>
+      <div className='footnote'>
+            1. <a href="https://www.goodreads.com/en/book/show/12622.Language_and_Mind">Chomsky's Language And The Mind</a>
+            2. <a href='https://www.goodreads.com/book/show/24113.G_del_Escher_Bach'>Godel, Escher, Bach</a>
+            3. <a href='https://www.astralcodexten.com/p/your-book-review-the-epic-of-gilgamesh'>A review of The Epic of Gilgamesh</a>
       </div>
     </div>
   );
